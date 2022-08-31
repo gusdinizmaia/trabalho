6ea183d24f8f61c78d7a32e7da21523d2ae5dc94
gusdinizmaia <gustavodiniz37@hotmail.com>
2022-08-31 17:11:14 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+let nome = "gustavo diniz";
+console.log (nome);
+console.log("Olá eu sou o " + (nome) + ", um prazer lhe conhecer");
+
+let nome2='gustavo'
+let sobrenome2='diniz'
+console.log (`${nome2} ${sobrenome2}`)
+
+let nome3 = 'gustavo'
+let sobrenome3 = 'diniz'
+let idade3 = '20'
+console.log (`eu sou ${nome3} ${sobrenome2}e tenho ${idade3} anos de idade`)
+
